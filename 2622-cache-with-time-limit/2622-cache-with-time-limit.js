d0aedfa86a4eeb626864732b585110db0ae6c05b
varshajangid07 <202263057+varshajangid07@users.noreply.github.com>
2026-07-25 00:27:45 +0530

    Time: 46 ms (71.14%), Space: 53.9 MB (32.51%) - LeetHub

---
+++ 2622-cache-with-time-limit/2622-cache-with-time-limit.js
@@ -0,0 +1,44 @@
+var TimeLimitedCache = function() {
+    this.cache=new Map();
+};
+
+/** 
+ * @param {number} key
+ * @param {number} value
+ * @param {number} duration time until expiration in ms
+ * @return {boolean} if un-expired key already existed
+ */
+TimeLimitedCache.prototype.set = function(key, value, duration) {
+    const exists=this.cache.has(key);
+    if(exists){
+        clearTimeout(this.cache.get(key).timerId);
+    }
+    const timerId=setTimeout(()=>{
+        this.cache.delete(key);
+    }, duration);
+    this.cache.set(key, { value : value, timerId : timerId });
+    return exists;
+};
+
+/** 
+ * @param {number} key
+ * @return {number} value associated with key
+ */
+TimeLimitedCache.prototype.get = function(key) {
+    if(this.cache.has(key)) return this.cache.get(key).value;
+    return -1;
+};
+
+/** 
+ * @return {number} count of non-expired keys
+ */
+TimeLimitedCache.prototype.count = function() {
+    return this.cache.size;
+};
+
+/**
+ * const timeLimitedCache = new TimeLimitedCache()
+ * timeLimitedCache.set(1, 42, 1000); // false
+ * timeLimitedCache.get(1) // 42
+ * timeLimitedCache.count() // 1
+ */
