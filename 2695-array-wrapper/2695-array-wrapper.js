64165f3ef0d0c841120053c03130942f5231f6b6
varshajangid07 <202263057+varshajangid07@users.noreply.github.com>
2026-08-04 22:39:30 +0530

    Time: 59 ms (5.41%), Space: 55.4 MB (15.32%) - LeetHub

---
+++ 2695-array-wrapper/2695-array-wrapper.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @return {void}
+ */
+var ArrayWrapper = function(nums) {
+    this.sum=nums.reduce((acc, curr)=>acc+curr, 0);
+    this.str=`[${nums.join(',')}]`;
+};
+
+/**
+ * @return {number}
+ */
+ArrayWrapper.prototype.valueOf = function() {
+    return this.sum;
+}
+
+/**
+ * @return {string}
+ */
+ArrayWrapper.prototype.toString = function() {
+    return this.str;
+}
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */
